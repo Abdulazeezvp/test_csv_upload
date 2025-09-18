f5cc28bcc86f6cc899678c07ac5fc7b5bdf0c4fe
Abdul Azeez <meazeez@gail.com>
2025-09-18 18:44:33 +0530

    fix:data insertion

---
+++ app/Helpers/SaveDataHelper.php
@@ -0,0 +1,54 @@
+<?php 
+
+namespace App\Helpers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Throwable;
+
+class SaveDataHelper{
+
+    public function __constrcut(){
+
+    }
+
+    public function saveData($file){
+        $handle = fopen($file->getPathname(), "r");
+        $response = [];
+        try{   
+            if ($handle) {
+                $header = fgetcsv($handle);
+                $data = [];
+                $pattern = '/^[a-zA-Z0-9]{3,20}$/';
+                while (($row = fgetcsv($handle)) !== FALSE) {                   
+                        $data[] = [
+                            'id'=>$row[0],
+                            'username'=>$row[1],
+                            'name'=>$row[1],
+                            'email'=>$row[2],
+                            'password'=>$row[3],
+                        ];
+                }
+                fclose($handle);
+            }
+            DB::beginTransaction();
+            $validator = Validator::make($data,[
+                '*'=>[
+                    'id'=>'unique:users,id',
+                    'username'=>'required|string|alpha_num|min:3|max:20|',
+                    'email'=>'required|email|unique:users,email',
+                    'password'=>'required'
+                ]
+            ]);
+            if(!$validator->fails()){
+                User::insertOrIgnore($data);
+            }else{
+                $response = $validator->validate();
+            }
+        }catch(Throwable $th){
+            $response = ['status'=>'error','message'=>$th->getMessage()];
+        }
+        return $response;
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,8 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
+        'id',
+        'username',
         'name',
         'email',
         'password',
